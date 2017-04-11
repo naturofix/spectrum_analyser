73af8f98ef01c14c364b36bcf444ac41dacdbb83
Shaun old mac <shaungarnett@gmail.com>
2017-04-11 13:30:43 +0200

    working with 200

--- Three_Strips_ada/Three_Strips_ada.ino
+++ Three_Strips_ada/Three_Strips_ada.ino
@@ -4,50 +4,108 @@
 #define BPIN 5
 #define MPIN 6
 #define TPIN 7
-#define NUM_LEDS    300
+#define NUM_LEDS 200
 
 
-Adafruit_NeoPixel strip_a = Adafruit_NeoPixel(NUM_LEDS, 5, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip_b = Adafruit_NeoPixel(NUM_LEDS, 6, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip_c = Adafruit_NeoPixel(NUM_LEDS, 7, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip_a = Adafruit_NeoPixel(NUM_LEDS, 5);
+Adafruit_NeoPixel strip_b = Adafruit_NeoPixel(NUM_LEDS, 6);
+Adafruit_NeoPixel strip_c = Adafruit_NeoPixel(NUM_LEDS, 7);
 
 void setup() {
+
   delay( 3000 );
-  strip_a.setBrightness(255);
+  
   strip_a.begin();
+  strip_a.setBrightness(25);
   strip_a.show();
   
-  strip_b.setBrightness(255);
+  
   strip_b.begin();
+  strip_b.setBrightness(25);
   strip_b.show();
   
-  strip_c.setBrightness(255);
+
   strip_c.begin();
+  strip_c.setBrightness(25);
   strip_c.show();
   delay( 3000 );
 
 }
 
 void loop() {
-  Serial.begin(57600);
-  int led_nums = 300;
+  int cd = 100;
+  int turn_off = 0;
+//  Serial.begin(57600);
+  int led_nums = NUM_LEDS;
   int delay_num = 5;
   int i;
   for(i=0; i<led_nums; i++) {
     strip_a.setPixelColor(i,255,0,0); 
   }
-  strip_a.show();
-  delay( 3000 );
+  if(turn_off == 0){
+    strip_a.show();
+  }
+  delay(cd);
    for(i=0; i<led_nums; i++) {
     strip_b.setPixelColor(i,0,255,0); 
   }
-  strip_b.show();
-  delay( 3000 );
+    if(turn_off == 0){
+    strip_b.show();
+  }
+  delay(cd);
   for(i=0; i<led_nums; i++) {
     strip_c.setPixelColor(i,0,0,255); 
   }
 
-  strip_c.show();
-  delay( 3000 );
+  if(turn_off == 0){
+    strip_c.show();
+  }
+  delay(cd);
+  
+    for(i=0; i<led_nums; i++) {
+    strip_a.setPixelColor(i,0,255,0); 
+  }
+  if(turn_off == 0){
+    strip_a.show();
+  }
+  delay(cd);
+   for(i=0; i<led_nums; i++) {
+    strip_b.setPixelColor(i,0,0,255); 
+  }
+    if(turn_off == 0){
+    strip_b.show();
+  }
+  delay(cd);
+  for(i=0; i<led_nums; i++) {
+    strip_c.setPixelColor(i,255,0,0); 
+  }
+
+  if(turn_off == 0){
+    strip_c.show();
+  }
+  delay(cd);
+  
+    for(i=0; i<led_nums; i++) {
+    strip_a.setPixelColor(i,0,0,255); 
+  }
+  if(turn_off == 0){
+    strip_a.show();
+  }
+  delay(cd);
+   for(i=0; i<led_nums; i++) {
+    strip_b.setPixelColor(i,255,0,0); 
+  }
+    if(turn_off == 0){
+    strip_b.show();
+  }
+  delay(cd);
+  for(i=0; i<led_nums; i++) {
+    strip_c.setPixelColor(i,0,255,0); 
+  }
+
+  if(turn_off == 0){
+    strip_c.show();
+  }
+  delay(cd);
   
 }
